45824c17f1aaeabfd80f24b37b89f61893dd1e83
schragman <schragman@gmail.com>
2022-12-28 08:07:15 +0100

    Einschränkung auf Roles

--- src/main/java/net/schrage/resourceserver/security/WebSecurity.java
+++ src/main/java/net/schrage/resourceserver/security/WebSecurity.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,13 +13,21 @@ public class WebSecurity {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
     http
         .authorizeHttpRequests((authz) -> authz
-            .requestMatchers(HttpMethod.GET, "/users/status/check").hasAuthority("SCOPE_profile")
+            .requestMatchers(HttpMethod.GET, "/users/status/check")
+            //.hasAuthority("SCOPE_profile")
+            //.hasAnyRole("Developer", "User")
+            .hasRole("Developer")
+            //.hasAuthority("ROLE_Developer")
             .anyRequest().authenticated()
         )
         .oauth2ResourceServer()
-        .jwt();
+        .jwt()
+        .jwtAuthenticationConverter(jwtAuthenticationConverter);
     return http.build();
   }
 
